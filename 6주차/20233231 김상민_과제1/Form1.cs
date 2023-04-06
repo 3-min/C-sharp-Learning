0ae8946079c667ae0e54945d038f1b52224d3731
3-min <Glaemfdj@oasis.inje.ac.kr>
2023-04-06 15:49:40 +0900

    6주차

---
+++ 6주차/20233231 김상민_과제1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20233231_김상민_과제1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = int.Parse(textBox1.Text);
+                int b = int.Parse(textBox2.Text);
+                int sum = 0;
+                int mul = 1;
+
+                if (a >= 1 && b <= 20)
+                {
+                    for(int i = a; i <= b; i++)
+                    {
+                        sum = sum + i;
+                        mul = mul * i;
+                    }
+                }
+                
+                label3.Text = a + "부터 " + b + "까지의 합은" + sum
+                    + "\n곱은 " + mul;
+            }
+            catch (Exception ex)
+            {
+                label3.Text = ex.Message;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ 6주차/20233231 김상민_과제2/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20233231_김상민_과제2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int iData01 = int.Parse(textBox1.Text);
+                string sData01 = "";
+                
+                for(int i=1; i <= iData01; i++)
+                {
+                    sData01 = sData01 + i + " , ";
+                    if (i % 7 == 0)
+                    {
+                        sData01 += "\n";
+                    }
+                }
+                sData01 = sData01.Substring(0, sData01.Length - 2);
+                label2.Text = "수행된 i값 : " + sData01;
+                
+            }
+            catch (Exception ex)
+            {
+                label2.Text = ex.Message;
+            }
+        }
+    }
+}
