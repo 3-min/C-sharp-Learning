ccd5ff18d9e16071a6d267ed1a22719e0d747b32
3-min <Glaemfdj@oasis.inje.ac.kr>
2023-04-06 21:23:58 +0900

    6주차

--- 6주차/20233231 김상민_과제1/Form1.cs
+++ 6주차/20233231 김상민_과제1/Form1.cs
@@ -21,22 +21,44 @@ namespace _20233231_김상민_과제1
         {
             try
             {
-                int a = int.Parse(textBox1.Text);
-                int b = int.Parse(textBox2.Text);
-                int sum = 0;
-                int mul = 1;
+                ulong a = ulong.Parse(textBox1.Text);
+                ulong b = ulong.Parse(textBox2.Text);
+                ulong sum = 0;
+                ulong mul = 1;
 
-                if (a >= 1 && b <= 20)
+               
+                    
+                if(a == b)
                 {
-                    for(int i = a; i <= b; i++)
+                    label3.Text = "시작값은 마지막값 보다 작은 값이어야 합니다!";
+                }
+                else if (a > b)
+                {
+                    label3.Text = "시작값은 마지막값 보다 작은 값이어야 합니다!";
+                }
+                else if (b > 20)
+                {
+                    label3.Text = "마지막값은 21보다 작은 값이어야 합니다.";
+                }
+                else if( 0 >= a)
+                {
+                    label3.Text = "시작값은 0보다 큰 값이야 합니다!";
+                }
+                
+                else
+                {
+                    for(ulong i = a; i <= b; i++)
                     {
                         sum = sum + i;
                         mul = mul * i;
                     }
+                    label3.Text = a + "부터 " + b + "까지의 합은" + sum
+                   + "\n곱은 " + mul;
                 }
                 
-                label3.Text = a + "부터 " + b + "까지의 합은" + sum
-                    + "\n곱은 " + mul;
+                
+               
+                
             }
             catch (Exception ex)
             {
@@ -47,6 +69,11 @@ namespace _20233231_김상민_과제1
         private void button2_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
