d3eb344883a1dc75771903bf51433527ea115ef6
3-min <Glaemfdj@oasis.inje.ac.kr>
2023-04-10 21:49:10 +0900

    중간고사 대비

---
+++ C# 중간고사 대비 문제/과제_4/과제_4/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 과제_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int num1 = int.Parse(textBox1.Text);
+                int num2 = int.Parse(textBox2.Text);
+                int check = 0;
+                int sum = 0;
+
+                label1.Text = "< 결과 출력 >\n\n";
+                for(int i =1; i <= num1; i++)
+                {
+                    if(i % num2 == 0)
+                    {
+                        label1.Text += i + " ";
+                        check++;
+                        sum++;
+                    }
+                    if(check == 5)
+                    {
+                        label1.Text += "\n\n";
+                        check = 0;
+                    }
+                }
+                label2.Text = num2 + " 배수의 갯수 : " + sum;
+            }
+            catch(Exception ex)
+            {
+                label1.Text = ex.Message;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
