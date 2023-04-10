495fb740c3e3a8974c9e5a579c3cfbe824f97616
3-min <Glaemfdj@oasis.inje.ac.kr>
2023-04-10 21:51:26 +0900

    2주차

---
+++ 2주차/20233231 김상민_과제 2/20233231 김상민_과제 2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20233231_김상민_과제_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sData01 = "20233231";
+            string sData02 = "홍길동";
+
+            label1.Text = "학번은 " + sData01 + "\n성명은 " + sData02;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sData03 = "010-7442-1262";
+            label3.Text = "연락처는" + sData03 + "입니다.";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
