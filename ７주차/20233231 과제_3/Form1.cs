153bf2f635905fbbfd6c168d18e74b7e13f1376e
3-min <Glaemfdj@oasis.inje.ac.kr>
2023-04-14 02:34:47 +0900

    7주차

---
+++ ７주차/20233231 과제_3/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20233231_과제_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(textBox1.Text);
+            int num2 = int.Parse(textBox2.Text);
+            int sum = 1;
+
+            label3.Text = "구구단 " + num1 + "부터 " + num2 + "단 까지입니다.\n\n";
+            try
+            {
+                if (num1 == num2 || num1 > num2)
+                {
+                    label3.Text = "Error";
+                }
+                else
+                {
+                    for (int i = num1; i <= num2; i++)
+                    {
+                        for (int y = 1; y <= 9; y++)
+                        {
+                            sum = i * y;
+                            label3.Text += i + " * " + y + " = " + sum + "\n";
+                            if (y % 9 == 0)
+                            {
+                                label3.Text += "\n";
+                            }
+                        }
+
+                    }
+                }
+                
+                
+            }
+            catch (Exception ex)
+            {
+                label3.Text = ex.Message;
+            }
+           
+        }
+    }
+}
---
+++ ７주차/20233231m 김상민_과제2/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20233231m_김상민_과제2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(textBox1.Text);
+            string str = "";
+
+            label2.Text = "";
+            try
+            {
+                if (num > 0 && num <= 100)
+                {
+                    for (int i = 1; i <= num; i++)
+                    {
+                        if (i % 3 == 0)
+                        {
+                            str += " 짝 ";
+                        }
+                        else
+                        {
+                            str += " " + i + " ";
+                        }
+                        if (i % 10 == 0)
+                        {
+                            str += "\n";
+                        }
+                    }
+                    label2.Text = str;
+                }
+                else
+                {
+                    label2.Text = "Error";
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                label2.Text = ex.Message;
+            }
+            
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            label2.Text = "결과가 출력되는 곳입니다.";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 4;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 5;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 6;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 7;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 8;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 9;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 0;
+        }
+    }
+}
